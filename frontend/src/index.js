6385df35fcb65f7564fb8a2162b40a2f2b3608a2
Revanth0749 <64368519+Revanth0749@users.noreply.github.com>
2025-05-04 19:42:24 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css"; // Optional, for global CSS styling
-import App from "./App"; // Import the main App component
-import reportWebVitals from "./reportWebVitals"; // Optional, for measuring performance
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-// This will render your entire React app inside the div with id="root" in index.html
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root") // This is where the React app is mounted in the HTML
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function to log results
-reportWebVitals();
